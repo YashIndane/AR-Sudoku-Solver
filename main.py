1da49032fae5ad2e6ac6ab6f5fc94ff9e5a47186
YashIndane <53041219+YashIndane@users.noreply.github.com>
2020-07-12 15:12:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from webcam_process import capture_save 
+from model_testing import generate_grid
+from sudoku3 import solve
+from motion_digits2 import fill_digits_motion
+import time
+
+
+coordinates = capture_save()
+
+GRID , empty_idx = generate_grid()
+
+print(GRID)
+
+time1 = time.time()
+
+num_array = solve(GRID)
+
+time2 = time.time()
+
+fill_digits_motion(num_array , coordinates , empty_idx , 'Solved in ' + '{:.3f}'.format(time2 - time1) + ' secs' )
+
+
+
+
+
+
+
